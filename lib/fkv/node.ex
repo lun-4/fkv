e84afd12eb250cf8b5a0bb79366f346897bb3234
Luna <git@l4.pm>
2021-02-08 15:37:23 -0300

    don't let PUTs happen on secondary nodes

--- lib/fkv/node.ex
+++ lib/fkv/node.ex
@@ -13,6 +13,10 @@ defmodule Fkv.Node do
     GenServer.call(pid, {:put, key, value})
   end
 
+  def put_from_primary(pid, key, value) do
+    GenServer.call(pid, {:put_from_primary, key, value})
+  end
+
   # -- impl --
 
   @impl true
@@ -44,11 +48,18 @@ defmodule Fkv.Node do
       Registry.dispatch(Fkv.Registry, "secondary", fn entries ->
         Enum.each(entries, fn datum ->
           {pid, _} = datum
-          Fkv.Node.put(pid, key, value)
+          Fkv.Node.put_from_primary(pid, key, value)
         end)
       end)
-    end
 
+      {:reply, :ok, %{state | map: Map.put(state.map, key, value)}}
+    else
+      {:reply, {:error, :not_primary_node}, state}
+    end
+  end
+
+  @impl true
+  def handle_call({:put_from_primary, key, value}, _from, state) do
     {:reply, :ok, %{state | map: Map.put(state.map, key, value)}}
   end
 end
