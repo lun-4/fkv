9d9517f6e26ace3c3194016e6d335c62ed35368c
Luna <git@l4.pm>
2021-02-08 18:00:43 -0300

    add some logs

--- lib/fkv/node.ex
+++ lib/fkv/node.ex
@@ -1,6 +1,8 @@
 defmodule Fkv.Node do
   use GenServer
 
+  require Logger
+
   def start_link(opts, genserver_opts \\ []) do
     GenServer.start_link(__MODULE__, opts, genserver_opts)
   end
@@ -17,37 +19,38 @@ defmodule Fkv.Node do
     GenServer.call(pid, {:delete, key})
   end
 
-  def put_from_primary(pid, key, value) do
-    GenServer.call(pid, {:put_from_primary, key, value})
-  end
-
-  def delete_from_primary(pid, key) do
-    GenServer.call(pid, {:delete_from_primary, key})
-  end
-
   # -- impl --
 
   @impl true
   def init(opts) do
     is_primary = Keyword.get(opts, :primary)
+    registry = Keyword.get(opts, :registry, Fkv.Registry)
 
     if is_primary do
-      {:ok, _} = Registry.register(Fkv.Registry, "primary", nil)
+      {:ok, _} = Registry.register(registry, "primary", nil)
     else
-      {:ok, _} = Registry.register(Fkv.Registry, "secondary", nil)
+      {:ok, _} = Registry.register(registry, "secondary", nil)
     end
 
     {:ok,
      %{
+       registry: registry,
        is_primary: is_primary,
-       map: %{}
+       map: %{},
+       replication_log: %{},
+       current_sequence_number: 0
      }}
   end
 
-  defp broadcast_secondaries(call_message) do
-    Registry.dispatch(Fkv.Registry, "secondary", fn entries ->
-      Enum.each(entries, fn datum ->
-        {pid, _} = datum
+  defp broadcast_secondaries(call_message, state) do
+    Registry.dispatch(state.registry, "secondary", fn entries ->
+      Enum.each(entries, fn {pid, _} ->
+        Logger.info(
+          "send replication change (primary=#{inspect(self())}): #{inspect(call_message)} to #{
+            inspect(pid)
+          }"
+        )
+
         GenServer.call(pid, call_message)
       end)
     end)
@@ -58,11 +61,34 @@ defmodule Fkv.Node do
     {:reply, Map.get(state.map, key), state}
   end
 
+  def do_operation_from_primary(operation, state) do
+    new_sequence_number = state.current_sequence_number + 1
+    broadcast_secondaries({:replication_log, new_sequence_number, operation}, state)
+
+    new_map =
+      case operation do
+        {:put, key, value} ->
+          Map.put(state.map, key, value)
+
+        {:delete, key} ->
+          Map.delete(state.map, key)
+      end
+
+    {
+      :reply,
+      :ok,
+      Map.merge(state, %{
+        map: new_map,
+        replication_log: Map.put(state.replication_log, new_sequence_number, operation),
+        current_sequence_number: new_sequence_number
+      })
+    }
+  end
+
   @impl true
   def handle_call({:put, key, value}, _from, state) do
     if state.is_primary do
-      broadcast_secondaries({:put_from_primary, key, value})
-      {:reply, :ok, %{state | map: Map.put(state.map, key, value)}}
+      do_operation_from_primary({:put, key, value}, state)
     else
       {:reply, {:error, :not_primary_node}, state}
     end
@@ -71,20 +97,37 @@ defmodule Fkv.Node do
   @impl true
   def handle_call({:delete, key}, _from, state) do
     if state.is_primary do
-      broadcast_secondaries({:delete_from_primary, key})
-      {:reply, :ok, %{state | map: Map.delete(state.map, key)}}
+      do_operation_from_primary({:delete, key}, state)
     else
       {:reply, {:error, :not_primary_node}, state}
     end
   end
 
   @impl true
-  def handle_call({:put_from_primary, key, value}, _from, state) do
-    {:reply, :ok, %{state | map: Map.put(state.map, key, value)}}
-  end
+  def handle_call({:replication_log, sequence_number, operation}, _from, state) do
+    if state.is_primary do
+      Logger.warn("WARNING! got replication log change but is primary")
+    end
 
-  @impl true
-  def handle_call({:delete_from_primary, key}, _from, state) do
-    {:reply, :ok, %{state | map: Map.delete(state.map, key)}}
+    Logger.info(
+      "got replication change (#{inspect(self())}): #{sequence_number} #{inspect(operation)}"
+    )
+
+    new_state =
+      Map.merge(state, %{
+        replication_log: Map.put(state.replication_log, sequence_number, operation),
+        current_sequence_number: sequence_number
+      })
+
+    new_map =
+      case operation do
+        {:put, key, value} ->
+          Map.put(state.map, key, value)
+
+        {:delete, key} ->
+          Map.delete(state.map, key)
+      end
+
+    {:reply, :ok, %{new_state | map: new_map}}
   end
 end
